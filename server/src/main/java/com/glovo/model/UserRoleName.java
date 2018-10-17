ab15841d4f10414ea8213f7f884430c0f66351fb
ASI\mlibosada <mlibosada@asi-dev5.com>
2018-10-17 15:11:32 +0800

    deleted unused resources

--- server/src/main/java/com/glovo/model/UserRoleName.java
+++
@@ -1,6 +0,0 @@
-package com.glovo.model;
-
-public enum UserRoleName {
-    ROLE_USER,
-    ROLE_ADMIN
-}
--- server/src/main/java/com/glovo/repository/AuthorityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.glovo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.glovo.model.Authority;
-
-public interface AuthorityRepository extends JpaRepository<Authority, Long> {
-  Authority findByName(String name);
-}
